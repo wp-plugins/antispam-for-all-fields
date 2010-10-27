604628e5b4dfa0fb6f9eca2ac685c715ec7de60a
ramon fincken <ramon fincken@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-10-27 17:38:22 +0000

    Version 0.4
    git-svn-id: https://plugins.svn.wordpress.org/antispam-for-all-fields/trunk@305048 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- antispam-for-all-fields.php
+++ antispam-for-all-fields.php
@@ -4,14 +4,51 @@ Plugin Name: Antispam for all fields
 Plugin URI: http://www.mijnpress.nl
 Description: Class and functions
 Author: Ramon Fincken
-Version: 0.3
+Version: 0.4
 Author URI: http://www.mijnpress.nl
 */
 
 add_filter('pre_comment_approved', 'plugin_antispam_for_all_fields', 0);
+
+// Disabled due to sessions, I want to store it otherwise ( I know that session_start() is an option )
+// add_action ('comment_form', 'plugin_antispam_for_all_fields_insertfields');
+// add_action ('comment_post', 'plugin_antispam_for_all_fields_checkfields');
+
 add_action('activity_box_end', 'plugin_antispam_for_all_fields_stats');
 
-define('PLUGIN_ANTISPAM_FOR_ALL_FIELDS_VERSION','0.3');
+define('PLUGIN_ANTISPAM_FOR_ALL_FIELDS_VERSION', '0.4');
+
+function plugin_antispam_for_all_fields_checkfields()
+{
+	$pass = false;
+	if(wp_verify_nonce($_POST[$_SESSION['plugin_afaf_nonce1']], 'plugin_afaf1') )
+	{
+		// Found first nonce, and was correct
+		$nonce2 = $_POST[$_SESSION['plugin_afaf_nonce1']];
+		if(!empty($nonce2) && $nonce2 == $_SESSION['plugin_afaf_nonce2'])
+		{
+			// Was correct
+			if(isset($_POST[$nonce2]) && empty($_POST[$nonce2]))
+			{
+				$pass = true;
+			}
+		}
+	}
+}
+
+function plugin_antispam_for_all_fields_insertfields()
+{
+	$nonce1= wp_create_nonce('plugin_afaf1');
+	$_SESSION['plugin_afaf_nonce1'] = $nonce1;
+	
+	set_transient('plugin_afaf_nonce1', $nonce1, 60*60); // 60*60 = 1hour
+	
+	
+	$nonce2= wp_create_nonce('plugin_afaf2');
+	$_SESSION['plugin_afaf_nonce2'] = $nonce2;
+	echo '<input type="hidden" name="'.$nonce1.'" value="'.$nonce2.'" />';
+	echo '<input type="hidden" name="'.$nonce2.'" value="" />';
+}
 
 /**
  * Displays stats in dashboard
@@ -19,10 +56,9 @@ define('PLUGIN_ANTISPAM_FOR_ALL_FIELDS_VERSION','0.3');
 function plugin_antispam_for_all_fields_stats() {
 	$statskilled = intval(get_option('plugin_antispam_for_all_fields_statskilled'));
 	$statsspammed = intval(get_option('plugin_antispam_for_all_fields_statsspammed'));
-	
-	echo '<p>'.sprintf(__('<a href="%1$s" target="_blank">Antispam for all fields</a> has blocked <strong>%2$s</strong> and spammed <strong>%2$s</strong> comments.'), 'http://wordpress.org/extend/plugins/antispam-for-all-fields/',  number_format($statskilled),number_format($statsspammed) ).'</p>';
-}
 
+	echo '<p>' . sprintf(__('<a href="%1$s" target="_blank">Antispam for all fields</a> has blocked <strong>%2$s</strong> and spammed <strong>%3$s</strong> comments.'), 'http://wordpress.org/extend/plugins/antispam-for-all-fields/', number_format($statskilled), number_format($statsspammed)) . '</p>';
+}
 
 function plugin_antispam_for_all_fields($status) {
 	global $commentdata;
@@ -40,8 +76,7 @@ class antispam_for_all_fields {
 	 * Core function to init spamchecks
 	 */
 	function init($status, $commentdata) {
-		if($commentdata['comment_type'] == 'trackback' || $commentdata['comment_type'] == 'pingback')
-		{
+		if ($commentdata['comment_type'] == 'trackback' || $commentdata['comment_type'] == 'pingback') {
 			return 0;
 		}
 		$this->lower_limit = 2;
@@ -84,53 +119,74 @@ class antispam_for_all_fields {
 					foreach ($words as $word) {
 						$string_is_spam = $this->string_is_spam($word, $html_body);
 						if ($string_is_spam) {
-								$body = "Details are below: \n";
-								$body .= "action: found spamword in URL of commenter, comment denied \n";
-					
-								$body .= "IP adress " . $this->user_ip . "\n";
-								$body .= "low threshold " . $this->lower_limit . "\n";
-								$body .= "upper threshold " . $this->upper_limit . "\n";			
-								
-								$body .= "word found  : " . $word . " \n\n";
-								
-								foreach ($commentdata as $key => $val) {
-									$body .= "$key : $val \n";
-								}
-										
-								$this->mail_details('rejected comment based on word',$body);
-							
-								Header('HTTP/1.1 403 Forbidden');
-								echo $word;
-								die('spam');
+							$body = "Details are below: \n";
+							$body .= "action: found spamword in URL of commenter, comment denied \n";
+
+							$body .= "IP adress " . $this->user_ip . "\n";
+							$body .= "low threshold " . $this->lower_limit . "\n";
+							$body .= "upper threshold " . $this->upper_limit . "\n";
+
+							$body .= "word found  : " . $word . " \n\n";
+
+							foreach ($commentdata as $key => $val) {
+								$body .= "$key : $val \n";
+							}
+
+							$this->mail_details('rejected comment based on word', $body);
+
+							Header('HTTP/1.1 403 Forbidden');
+							echo $word;
+							die('spam');
 						}
 					}
 				}
 			}
 		}
 		if (!empty ($comment_content)) {
+			// 
+			$number_of_sites = $this->count_number_of_sites($comment_content);
+			if($number_of_sites > 10)
+			{
+				$body = "Details are below: \n";
+				$body .= "action: found ".$number_of_sites. " URIs in comment that is a lot, comment marked as spam \n";
+
+				$body .= "IP adress " . $this->user_ip . "\n";
+				$body .= "low threshold " . $this->lower_limit . "\n";
+				$body .= "upper threshold " . $this->upper_limit . "\n";
+
+				foreach ($commentdata as $key => $val) {
+					$body .= "$key : $val \n";
+				}
+				
+				$this->mail_details('rejected spammed based on '.$number_of_sites. ' URIs in comment', $body);
+				$temp = get_option('plugin_antispam_for_all_fields_statsspammed');
+				update_option('plugin_antispam_for_all_fields_statsspammed', intval($temp) + 1);			
+				return 'spam';
+			}
+			
 			foreach ($words as $word) {
 				$string_is_spam = $this->string_is_spam($word, $comment_content);
 				if ($string_is_spam) {
-			
+
 					$body = "Details are below: \n";
 					$body .= "action: found spamword in comment, comment denied \n";
-		
+
 					$body .= "IP adress " . $this->user_ip . "\n";
 					$body .= "low threshold " . $this->lower_limit . "\n";
-					$body .= "upper threshold " . $this->upper_limit . "\n";			
-					
+					$body .= "upper threshold " . $this->upper_limit . "\n";
+
 					$body .= "word found  : " . $word . " \n\n";
-					
+
 					foreach ($commentdata as $key => $val) {
 						$body .= "$key : $val \n";
 					}
-							
-					$this->mail_details('rejected comment based on word',$body);
-				
+
+					$this->mail_details('rejected comment based on word', $body);
+
 					Header('HTTP/1.1 403 Forbidden');
 					echo $word;
 					die('spam');
-				
+
 				}
 			}
 		}
@@ -163,10 +219,10 @@ class antispam_for_all_fields {
 	private function get_words() {
 		$words = array (
 			'*.ru*',
-			// '*sex*', positive for sex and the city
-			'*pharmac*',
-			// '*CIALIS*', positive for gespecialiseerd
-			'*viagra*',
+				// '*sex*', positive for sex and the city
+	'*pharmac*',
+				// '*CIALIS*', positive for gespecialiseerd
+	'*viagra*',
 			'*mortgage*',
 			'*drug*',
 			'*blogspot.com*',
@@ -211,18 +267,17 @@ class antispam_for_all_fields {
 	 * Notifies admin or custom inserted replacement > get_option('plugin_antispamfaf_emaillog')
 	 * TODO: Make admin option page
 	 */
-	private function mail_details($subject, $body)
-	{
-			$admin_email = get_option('admin_email');
-			$log_email = get_option('plugin_antispamfaf_emaillog');
-			$blogname = get_option('blogname');
-			if (isset ($log_email) && !empty ($log_email)) {
-				$email_to = $log_email;
-			} else {
-				$email_to = $admin_email;
-			}
-			$body.="\n\nAntispam for all fields by Ramon Fincken\nhttp://wordpress.org/extend/plugins/profile/ramon-fincken";
-			wp_mail($email_to, '[' . $blogname . '][Antispam] '.$subject.' ' . date('r'), $body);
+	private function mail_details($subject, $body) {
+		$admin_email = get_option('admin_email');
+		$log_email = get_option('plugin_antispamfaf_emaillog');
+		$blogname = get_option('blogname');
+		if (isset ($log_email) && !empty ($log_email)) {
+			$email_to = $log_email;
+		} else {
+			$email_to = $admin_email;
+		}
+		$body .= "\n\nAntispam for all fields by Ramon Fincken\nhttp://wordpress.org/extend/plugins/profile/ramon-fincken";
+		wp_mail($email_to, '[' . $blogname . '][Antispam] ' . $subject . ' ' . date('r'), $body);
 	}
 
 	/**
@@ -241,10 +296,10 @@ class antispam_for_all_fields {
 			foreach ($commentdata as $key => $val) {
 				$body .= "$key : $val \n";
 			}
-			
-			$this->mail_details('rejected comment',$body);
+
+			$this->mail_details('rejected comment', $body);
 			$temp = get_option('plugin_antispam_for_all_fields_statskilled');
-			update_option('plugin_antispam_for_all_fields_statskilled',intval($temp)+1);
+			update_option('plugin_antispam_for_all_fields_statskilled', intval($temp) + 1);
 			Header('HTTP/1.1 403 Forbidden');
 			die('spam');
 		} else {
@@ -260,11 +315,11 @@ class antispam_for_all_fields {
 				foreach ($commentdata as $key => $val) {
 					$body .= "$key : $val \n";
 				}
-								
-				$this->mail_details('spammed comment',$body);
+
+				$this->mail_details('spammed comment', $body);
 
 				$temp = get_option('plugin_antispam_for_all_fields_statsspammed');
-				update_option('plugin_antispam_for_all_fields_statsspammed',intval($temp)+1);
+				update_option('plugin_antispam_for_all_fields_statsspammed', intval($temp) + 1);
 				return 'spam';
 			}
 		}
@@ -274,23 +329,68 @@ class antispam_for_all_fields {
 	/**
 	 * Performs bugfix
 	 */
-	function do_bugfix()
-	{
+	function do_bugfix() {
 		// Bugfix for v 0.2
-		if(PLUGIN_ANTISPAM_FOR_ALL_FIELDS_VERSION == '0.3' && get_option('plugin_antispam_for_all_fields_v02fix') != 'yes')
-		{
+		if ((PLUGIN_ANTISPAM_FOR_ALL_FIELDS_VERSION == '0.3' || PLUGIN_ANTISPAM_FOR_ALL_FIELDS_VERSION == '0.4') && get_option('plugin_antispam_for_all_fields_v02fix') != 'yes') {
 			global $wpdb;
-			
+
 			$sql = 'UPDATE ' . $wpdb->comments . ' SET comment_approved = 0 WHERE comment_approved = \'\'';
 			$wpdb->get_results($sql, ARRAY_A);
-			update_option( 'plugin_antispam_for_all_fields_v02fix', 'yes');
+			update_option('plugin_antispam_for_all_fields_v02fix', 'yes');
 		}
-		if(PLUGIN_ANTISPAM_FOR_ALL_FIELDS_VERSION == '0.4')
-		{
-			delete_option('plugin_antispam_for_all_fields_v02fix');
-		}
-		// Bugfix for v 0.2		
+		// Bugfix for v 0.2
 	}
+
+	// ---------------- SYNTAX TEST FUNCTIONS
+	/**
+	 * Counts occurences of webadresses (including [url])
+	 */
+	private function count_number_of_sites($txt) {
+		// 1.2.9
+		// http://phpbbantispam.com/viewtopic.php?t=129
+		// [url=somesites.nw]Some site[/url] becomes http://somesites.nw https://somesites.nw Some site
+		$txt = preg_replace('/\[url=([^http](.+))\](.+)\[\/(.+)\]/', "http://$1 https://$1 $3", $txt);
+
+		// 1.2.7 Check max websites
+		// Partially re-coded from : http://www.phpbb.com/community/viewtopic.php?f=16&t=360188&start=30&st=0&sk=t&sd=a   
+		$out = array ();
+		preg_match_all("/http:\/\/|ftp:\/\/|[A-Z0-9._%-]+@[A-Z0-9.-]+\.[A-Z]{2,4}/si", $txt, $out, PREG_SET_ORDER);
+		// Removed |www
+		$number = count($out);
+		return $number;
+	}
+
+	function website_syntax_ok($url) {
+		$url = strtolower($url);
+		if (empty ($url))
+			return false;
+		if (!preg_match('#^http[s]?\\:\\/\\/[a-z0-9\-]+\.([a-z0-9\-]+\.)?[a-z]+#i', $url)) {
+			return false;
+		}
+		$pattern = '/\[url/';
+		preg_match($pattern, $url, $matches, PREG_OFFSET_CAPTURE);
+		if (count($matches) > 0) {
+			return false;
+		}
+		return true;
+	}
+
+	function mail_syntax_ok($mail) {
+		if (empty ($mail))
+			return false;
+		@ list ($local, $host) = explode("@", $mail);
+		$pattern_local = "^([0-9a-z]*([-|_]?[0-9a-z]+)*)(([-|_]?)\.([-|_]?)[0-9a-z]*([-|_]?[0-9a-z]+)+)*([-|_]?)$";
+		$pattern_host = "^([0-9a-z]+([-]?[0-9a-z]+)*)(([-]?)\.([-]?)[0-9a-z]*([-]?[0-9a-z]+)+)*\.[a-z]{2,4}$";
+		$match_local = eregi($pattern_local, $local);
+		$match_host = eregi($pattern_host, $host);
+
+		if ($match_local && $match_host) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	// ---------------- SYNTAX TEST FUNCTIONS
 
 	// ---------------- CHANGE STRING FUNCTIONS
 	/**
@@ -431,7 +531,7 @@ class antispam_for_all_fields {
 		}
 		return $txt;
 	}
-	
+
 	/**
 	 * Strips double chars
 	 * Partial/rewrote code from: Forum Assassin, Dom Walenczak http://spam.wulfslair.com/ (Cybertronian Alliance Corp)
--- readme.txt
+++ readme.txt
@@ -1,18 +1,22 @@
 === Antispam for all fields ===
 Contributors: Ramon Fincken
 Donate link: http://donate.ramonfincken.com
-Tags: spam,antispam,phpbbantispam,anti-spam
+Tags: spam,antispam,phpbbantispam,anti-spam,wordpressantispam
 Requires at least: 2.0.2
 Tested up to: 3.0.1
-Stable tag: 0.3
+Stable tag: 0.4
 
 Plugin to reject spam. Port from same author from http://www.phpbbantispam.com
 Actually visits the URL from commenter to spider for spamwords. Plugin does a lot more.
 
 == Description ==
 
-Plugin to reject spam. Port from same author from http://www.phpbbantispam.com
-Actually visits the URL from commenter to spider for spamwords. Plugin does a lot more.
+Plugin to reject spam. Port from same author from http://www.phpbbantispam.com <br>
+Actually visits the URL from commenter to spider for spamwords. <br>
+Plugin does a lot more such as:<br>
+* Count for number of web-URI's in comment<br>
+* Count on email, IP, URI compared with allready spammed comments<br>
+* Future feature: Add hidden fields with random names
 
 == Installation ==
 
@@ -25,6 +29,10 @@ None available
 
 
 == Changelog ==
+
+= 0.4 =
+Bugfix: plugin_antispam_for_all_fields_stats for spammed stats
+Added: Check for number of websites in comment, if above 10 then spam comment
 
 = 0.3 =
 Bugfix: forgot to report status, fix that will run once is included.
